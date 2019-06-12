b951e2913b955292630ff18a5721526a33939283
Tyler <williamstyler707@gmail.com>
2019-06-12 00:28:48 -0400

    moved server.js

---
+++ FriendFinder/app/server.js
@@ -0,0 +1,33 @@
+var express = require("express"),
+
+    peopleArray = require("../FriendFinder/app/data/friends"),
+    app = express(),
+
+
+    PORT = process.env.PORT || 8080
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+// hde
+app.post("/api/friends", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newperson = req.body;
+
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    console.log("New Entry: " + newperson.score);
+    console.log("People: "+peopleArray);
+    peopleArray.push(newperson);
+
+    res.json(newperson);
+
+});
+
+app.listen(PORT, function () {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
