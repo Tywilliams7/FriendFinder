1f7688d21fedfc7758e21220b549ae322cb298c4
Tyler <williamstyler707@gmail.com>
2019-06-14 22:24:58 -0400

    Compared

--- app/data/friends.js
+++ app/data/friends.js
@@ -23,25 +23,16 @@ var peopleArray = [
             "2", "5",
             "4", "1"
         ]
+    }, {
+        name: "Mia Vobos",
+        photo: "",
+        scores: [
+            "2", "1",
+            
+        ]
     }
 ]
 
-// for (i = 0; i < length; i++){
-// var length = peopleArray.length,
-    
-// }
 
-
-
-// for (i = 0; i < length; i++) {
-//     var length = peopleArray.length;
-//     var Scores = peopleArray[i].scores;
-//     var res = Scores.reduce(function (prev, curr) {
-//         return (Number(prev)) + (Number(curr));
-//     });
-// }
-// console.log(length);
-
-// console.log(res); // prints 32 for Hector
 
 module.exports = peopleArray;
--- server.js
+++ server.js
@@ -16,11 +16,56 @@ app.post("/api/friends", function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newperson = req.body;
+    var total = 0;
+    var totals = []
 
     // Using a RegEx Pattern to remove spaces from newCharacter
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
     console.log("New Entry: " + newperson.score);
-    console.log("People: "+peopleArray);
+    console.log("People: " + peopleArray);
+    console.log(newperson.score.length);
+
+    // New User total
+    for (i = 0; i < newperson.score.length; i++) {
+        total += Number(newperson.score[i]);
+    }
+    console.log("New User total: " + total);
+
+
+    // PeopleArr total
+    const result = peopleArray.map((value) => {
+        return {
+            name: value.name,
+            score: value.scores.reduce((total, score) => total + Number(score), 0)
+        }
+
+    });
+    console.log(result);
+    console.log("length: " + result.length);
+
+    // Comparing number to array
+
+
+    for (let i = 0; i < result.length; i++) {
+
+        totals.push(result[i].score);
+
+        if (total == result[i].score) {
+            console.log("Exact Match: " + result[i])
+        } else {
+            var closest = totals.reduce(function (prev, curr) {
+                return (Math.abs(curr - total) < Math.abs(prev - total) ? curr : total);
+            });
+        }
+    }
+    c = Number(closest);
+    console.log("Index: " + c);
+    console.log(result[c]);
+
+
+
+    console.log(totals)
+
     peopleArray.push(newperson);
 
     res.json(newperson);
